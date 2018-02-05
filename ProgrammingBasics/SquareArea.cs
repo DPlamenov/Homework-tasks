31b2d36e1123cd18ff5f0c2a0e53ed12480cee47
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2018-02-05 10:04:47 +0200

    added solutions
    added solutions

---
+++ ProgrammingBasics/SquareArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a = ");
+            var a = int.Parse(Console.ReadLine());
+            var area = a * a;
+            Console.Write("Square = ");
+            Console.WriteLine(area);
+
+        }
+    }
+}
