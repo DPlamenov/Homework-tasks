c4f974668a461278bb455edbacb8e53125f3946c
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2018-08-21 10:12:36 +0300

    add some solutions
    add some solutions

---
+++ ProgrammingFundamentals/AddTwoNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var sum = a + b;
+            var result = $"{a} + {b} = {sum}";
+            Console.WriteLine(result);
+        }
+    }
+}
