874e7d4eff968047390a02a52f1286529259e0f4
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2018-08-22 08:50:40 +0300

    first php homework finished
    first php homework finished

--- Web/php/01/new.php
+++ Web/php/01/new.php
@@ -1,19 +1,31 @@
 <?php
 require_once 'config.php';
 ?>
-<a href="index.php">Разходи</a>
-<form>
-    <input type="text" name="razhod" placeholder="Разхода"/>
-    <br>
-    <input type="text" name="cena" placeholder="Цена"/>
-    <select>
-        <?php
-        foreach ($types as $type){
-            echo "<option>".$type."</option>";
-        }
+    <a href="index.php">Разходи</a>
+    <form method="post">
+        <input type="text" name="razhod" placeholder="Разхода"/>
+        <br>
+        <input type="text" name="cena" placeholder="Цена"/>
+        <label>
+            <select name="tip">
+                <?php
+                foreach ($types as $key => $type) {
+                    echo "<option value = '" . $key . "'>" . $type . "</option>";
+                }
 
-        ?>
-    </select>
-    <input type="submit" name="ok"/>
-</form>
+                ?>
+            </select>
+        </label>
+        <input type="submit" name="ok"/>
+    </form>
 <?php
+if (isset($_POST['razhod'])) {
+    $razhod = trim($_POST['razhod']);
+    $cena = trim($_POST['cena']);
+    $tip = trim($_POST['tip']);
+
+    $toWrite = $razhod."!".$cena."!".$tip.PHP_EOL;
+    file_put_contents($file,$toWrite,FILE_APPEND);
+    header("Location: index.php");
+    exit;
+}
