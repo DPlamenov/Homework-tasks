2fb3d63a34aafbe1c5b5aab1afd750b2d7595a31
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2018-08-25 16:20:31 +0300

    Create jframe.java

---
+++ ProgrammingFundamentals/jframe.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame windows = new JFrame("Program");
+        windows.setVisible(true);
+        windows.setSize(800,600);
+        windows.setResizable(false);
+        JLabel welcome_label = new JLabel("Welcome");
+        welcome_label.setHorizontalAlignment(SwingConstants.CENTER);
+        welcome_label.setVerticalAlignment(SwingConstants.TOP);
+        windows.add(welcome_label);
+    }
+}
