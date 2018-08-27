a7697736ab22b32b1d0713e74f1a232f0e7b7c54
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2018-08-27 16:21:00 +0300

    Update animate.js

--- Web/JavaScript/animate.js
+++ Web/JavaScript/animate.js
@@ -1,9 +1,9 @@
 $('document').ready(function () {
     console.log("start");
     $( "#animate" ).animate({
-        height: "100px",
-        width: "200px"
-    }, 5000, function() {
+        height: "110px",
+        width: "170px"
+    }, 4000, function() {
         console.log("ready");
     });
 });
