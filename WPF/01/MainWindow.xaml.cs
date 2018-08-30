fbb7f52aecc18cae17c4ba776d8ef1eb21a914f6
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2018-08-30 17:49:07 +0300

    and c# wpf project
    and c# wpf project

---
+++ WPF/01/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace WpfApp1
+{
+
+    public partial class MainWindow : Window
+    {
+      
+
+        //Main method
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");            
+        }
+
+
+        
+    }
+}
