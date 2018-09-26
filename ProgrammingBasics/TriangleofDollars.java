ab9a2ae4170f284ff2cf7e3d03abfbb51a1564ad
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2018-09-26 07:21:55 +0300

    Create TriangleofDollars.java

---
+++ ProgrammingBasics/TriangleofDollars.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+       int n = Integer.parseInt(scanner.nextLine());
+       String dollars = "$";
+
+
+
+    }
+}
