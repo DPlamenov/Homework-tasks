649290a597abc1879fd9f5b0998b1c0c8f589f13
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2019-07-15 10:09:28 +0300

    Update NameGame.java

--- ProgrammingBasics/NameGame.java
+++ ProgrammingBasics/NameGame.java
@@ -2,9 +2,9 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class NameGame {
 
-    public static void NameGame(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = "";
         String winner = "";
