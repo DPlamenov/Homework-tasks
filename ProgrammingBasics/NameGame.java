b8838a856833605c57f51ed068e37eabecb38539
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2019-07-15 09:58:29 +0300

    Create NameGame.java

---
+++ ProgrammingBasics/NameGame.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void NameGame(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        String winner = "";
+        double winner_points = 0;
+        while (!"Stop".equals(name = scanner.nextLine())) {
+            int _point = 0;
+            for (int i = 0; i < name.length(); i++){
+                int num = Integer.parseInt(scanner.nextLine());
+                if(name.charAt(i) == num){
+                    _point += 10;
+                }else{
+                    _point += 2;
+                }
+            }
+            if(winner_points <= _point){
+                winner_points = _point;
+                winner = name;
+            }
+        }
+        System.out.printf("The winner is %s with %.0f points!", winner, winner_points);
+    }
+}
