7932b4231ba23b2c46faffce1e8ad2265e234b01
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2019-07-17 10:20:08 +0300

    Create MovieTicket.java

---
+++ ProgrammingBasics/MovieTicket.java
@@ -0,0 +1,23 @@
+package pb.homework;
+
+import java.util.Scanner;
+
+public class MovieTicket {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a1 = Integer.parseInt(scanner.nextLine());
+        int a2 = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = a1; i <= a2 - 1; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                for (int k = 1; k <= n / 2 - 1; k++) {
+                    int test = j + k + i;
+                    if (i % 2 != 0 && test % 2 != 0) {
+                        System.out.printf("%c-%d%d%d%n", i, j, k, (int) i);
+                    }
+                }
+            }
+        }
+    }
+}
