a50d75799e4e44b81c4c6106003d34199f06f4af
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-01-18 10:13:15 +0200

    add some js solutions

--- FundamentalsModule/JavaScriptPlayground.js
+++ FundamentalsModule/JavaScriptPlayground.js
@@ -2,5 +2,6 @@ let regexp = /[\d]+\.[\d]{2}/gi;
 let string = "Buy it only for 50.00 BGN";
 console.log(regexp.exec(string));
 
-let array = [];
+let array = [2, 5, 6, 50 , 1, 5, 5, 6, 1];
+let output = [];
 
