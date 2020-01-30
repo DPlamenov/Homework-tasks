faf84d5c6d79780e428eb78f51e50ddbf28c0d23
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-01-30 16:29:24 +0200

    add some js solutions

--- FundamentalsModule/JavaScriptPlayground.js
+++ FundamentalsModule/JavaScriptPlayground.js
@@ -1,7 +1,8 @@
-Array.prototype.map = function (callback) {
-    let resultArray = [];
-    this.forEach(function (element, index) {
-        resultArray[index] = callback(element, index);
-    });
-    return resultArray;
+let array = [20, 60, 10];
+
+let index = 6;
+while (index > array.length - 1) {
+    index -= array.length;
 }
+console.log(index);
+console.log(array[index]);
