8734576aa26a916134c86714d5da1bebed603363
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-04-17 14:50:24 +0300

    add some js solutions

---
+++ JavaScriptDevelopment/JS Advanced/JS Advanced/ArrayExercise/Orbit.js
@@ -0,0 +1,16 @@
+function solve(input) {
+    let [width, height, x, y] = input;
+
+    let matrix = [];
+    for (let i = 0; i < width; i++) {
+        matrix.push([]);
+    }
+
+    for (let row = 0; row < width; row++) {
+        for (let col = 0; col < height; col++) {
+            matrix[row][col] = Math.max(Math.abs(row - x), Math.abs(col - y)) + 1;
+        }
+    }
+
+    console.log(matrix.map(row => row.join(" ")).join("\n"));
+}
