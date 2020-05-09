bbd0b6903c291c58a60184dd610c69081b4437d1
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-05-09 13:58:12 +0300

    add some js solutions

--- JavaScriptDevelopment/JS Advanced/JS Advanced/DOMExercise/08. Encode-and-Decode-Messages/app.js
+++ JavaScriptDevelopment/JS Advanced/JS Advanced/DOMExercise/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeBtn = Array.from(document.querySelectorAll('button'))[0];
+    let decodeBtn = Array.from(document.querySelectorAll('button'))[1];
+
+
+    encodeBtn.addEventListener('click', encode);
+    decodeBtn.addEventListener('click', decode);
+
+    function encode(e) {
+        let inputTextArea = Array.from(document.querySelectorAll('div textarea'))[0];
+        let outputTextArea = Array.from(document.querySelectorAll('div textarea'))[1];
+        let textToEncode = inputTextArea.value;
+        outputTextArea.value = textToEncode.split('')
+            .map(e => String.fromCharCode(e.charCodeAt(0) + 1))
+            .join('');
+
+        inputTextArea.value = '';
+    }
+
+    function decode(e) {
+        let textArea = Array.from(document.querySelectorAll('div textarea'))[1];
+        let decoded = textArea.value
+            .split('')
+            .map(e => String.fromCharCode(e.charCodeAt(0) - 1));
+
+        textArea.value = decoded.join('');
+    }
 }
--- JavaScriptDevelopment/JS Advanced/JS Advanced/DOMExercise/09. Distance-Converter/distanceConverter.js
+++ JavaScriptDevelopment/JS Advanced/JS Advanced/DOMExercise/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,50 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let convertBtn = document.querySelector('#convert');
+    convertBtn.addEventListener('click', convert);
+
+    function convert(e) {
+        let convertFrom = document.querySelector('#inputUnits').value;
+        let convertTo = document.querySelector('#outputUnits').value;
+
+        let inputDistance = document.querySelector('#inputDistance').value;
+        let outputElement = document.querySelector('#outputDistance');
+        let outputToMeters = 0;
+        if (convertFrom === 'km') {
+            outputToMeters = inputDistance * 1000;
+        } else if (convertFrom === 'm') {
+            outputToMeters = inputDistance * 1;
+        } else if (convertFrom === 'cm') {
+            outputToMeters = inputDistance / 100;
+        } else if (convertFrom === 'mm') {
+            outputToMeters = inputDistance / 100 / 10;
+        } else if (convertFrom === 'mi') {
+            outputToMeters = inputDistance * 1609.34;
+        } else if (convertFrom === 'yrd') {
+            outputToMeters = inputDistance * 0.9144;
+        } else if (convertFrom === 'ft') {
+            outputToMeters = inputDistance * 0.3048;
+        } else if (convertFrom === 'in') {
+            outputToMeters = inputDistance * 0.0254;
+        }
+        let result = 0;
+        if (convertTo === 'km') {
+            result = outputToMeters / 1000;
+        } else if (convertTo === 'm') {
+            result = outputToMeters;
+        } else if (convertTo === 'cm') {
+            result = outputToMeters * 100;
+        } else if (convertTo === 'mm') {
+            result = outputToMeters * 100 * 10;
+        } else if (convertTo === 'mi') {
+            result = outputToMeters * 0.000621371192;
+        } else if (convertTo === 'yrd') {
+            result = outputToMeters * 1.0936133;
+        } else if (convertTo === 'ft') {
+            result = outputToMeters * 3.2808399;
+        } else if (convertTo === 'in') {
+            result = outputToMeters * 39.3700787;
+        }
+        alert(result.toFixed(3));
+        outputElement.value = result;
+    }
 }
