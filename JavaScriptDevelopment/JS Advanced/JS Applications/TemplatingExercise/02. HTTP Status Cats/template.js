7811f9611f06df0e19d8b63ddfa62ead1c23a4f5
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-07-15 08:38:20 +0300

    handlebars exercise

--- JavaScriptDevelopment/JS Advanced/JS Applications/TemplatingExercise/02. HTTP Status Cats/template.js
+++ JavaScriptDevelopment/JS Advanced/JS Applications/TemplatingExercise/02. HTTP Status Cats/template.js
@@ -1,8 +1,31 @@
-(() => {
-     renderCatTemplate();
+(async () => {
+    const allCats = document.querySelector('#allCats');
 
-     function renderCatTemplate() {
-          // TODO: Render cat template and attach events
-     }
+    const templateCatsText = await fetch('cats.hbs')
+        .then(response => response.text());
+    Handlebars.registerPartial('cat', document.querySelector('#cat-template').innerHTML);
+    const templateCats = Handlebars.compile(templateCatsText);
 
-});
+    renderCatTemplate();
+
+    allCats.addEventListener('click', function (e) {
+        if (e.target.tagName === 'BUTTON') {
+            const button = e.target;
+            const hiddenDiv = button.parentNode.querySelector('div');
+            toggleElement(hiddenDiv);
+        }
+    });
+
+    function toggleElement(el) {
+        if (el.style.display !== 'none') {
+            el.style.display = 'none';
+        } else {
+            el.style.display = 'block';
+        }
+    }
+
+    function renderCatTemplate() {
+        allCats.innerHTML = templateCats({ cats });
+
+    }
+})();
--- JavaScriptDevelopment/JS Advanced/JS Applications/TemplatingExercise/03. Popular Monkeys/monkeysTemplate.js
+++ JavaScriptDevelopment/JS Advanced/JS Applications/TemplatingExercise/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,21 @@
 $(() => {
-    // TODO
+    const monkeyOutput = document.querySelector('div.root');
+    const monkeyTemplate = document.querySelector('#monkey-template').innerHTML;
+    const monkeysTemplate = document.querySelector('#monkeys').innerHTML;
+    const monkeyRenderFunction = Handlebars.compile(monkeysTemplate);
+
+    monkeyOutput.addEventListener('click', showInfo);
+
+    Handlebars.registerPartial('monkey', monkeyTemplate);
+    monkeyOutput.innerHTML = monkeyRenderFunction({ monkeys });
 });
+
+function showInfo(e) {
+    const { target } = e;
+
+    if (target.tagName === 'BUTTON') {
+        const hiddenParagraph = target.parentNode.querySelector('p');
+        hiddenParagraph.style.display = 'block';
+    }
+}
+
