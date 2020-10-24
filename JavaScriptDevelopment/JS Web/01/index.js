9cd36fd2a434ff227ca38718edd5fab038767d65
dplamenov <dimitar.plamenov@gmail.com>
2020-10-24 14:28:48 +0300

    url module demo

--- JavaScriptDevelopment/JS Web/01/index.js
+++ JavaScriptDevelopment/JS Web/01/index.js
@@ -5,12 +5,16 @@
 // });
 
 //local modules
-const {printWithAsterisk} = require('./utils');
-printWithAsterisk('hello');
+// const {printWithAsterisk} = require('./utils');
+// printWithAsterisk('hello');
 
 //third-party
-const _ = require('lodash');
-
-const array = [1, 5, 10];
-const lastElement = _.last(array);
-printWithAsterisk(lastElement);
+// const _ = require('lodash');
+//
+// const array = [1, 5, 10];
+// const lastElement = _.last(array);
+// printWithAsterisk(lastElement);
+//
+// const url = require('url');
+// const urlObject = url.parse('http://localhost:3000/user/register?z=1&name=test', true);
+// const {hostname, query, pathname} = urlObject;
