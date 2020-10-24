a3a629ad64f0ee3004044fa70d0bc8daa320d29b
dplamenov <dimitar.plamenov@gmail.com>
2020-10-24 18:12:56 +0300

    setup testing with jest

---
+++ JavaScriptDevelopment/testing/index.spec.js
@@ -0,0 +1,10 @@
+// test('two plus two is four', () => {
+//   expect(2 + 2).toBe(4);
+// });
+//
+// test('two minus one is one', () => {
+//   expect(2 - 1).toBe(1);
+// });
+
+const index = require('./math');
+
