a68b66aa7675e678adab99a633ecc5da7571c4ab
dplamenov <dimitar.plamenov@gmail.com>
2021-01-13 14:49:36 +0200

    Typography CSS
    Typography CSS

---
+++ HTML&CSS/CSS & TYPOGRAPHY/IconFontList/script.js
@@ -0,0 +1,32 @@
+// let uls = $("body ul.icons");
+// let listItems1 = $(uls[0]).children();
+// let listItems2 = $(uls[1]).children();
+// let listItems3 = $(uls[2]).children();
+//
+// let listItem1 = $(listItems1[0]);
+// let listItem2 = $(listItems1[1]);
+// let listItem3 = $(listItems1[2]);
+//
+// let listItem4 = $(listItems2[0]);
+// let listItem5 = $(listItems2[1]);
+// let listItem6 = $(listItems2[2]);
+//
+// let listItem7 = $(listItems3[0]);
+// let listItem8 = $(listItems3[1]);
+// let listItem9 = $(listItems3[2]);
+//
+// let index1 = listItem1.text().indexOf('Phasellus dictum interdum efficitur');
+// let index2 = listItem2.text().indexOf('Maecenas eleifend sapien felis, quis interdum ex tempor a');
+// let index3 = listItem3.text().indexOf('Aenean sodales, eros ac fermentum elementum, enim nisi finibus nisi, sed tempus turpis mauris ac quam');
+//
+// let index4 = listItem4.text().indexOf('Phasellus dictum interdum efficitur');
+// let index5 = listItem5.text().indexOf('Maecenas eleifend sapien felis, quis interdum ex tempor a');
+// let index6 = listItem6.text().indexOf('Aenean sodales, eros ac fermentum elementum, enim nisi finibus nisi, sed tempus turpis mauris ac quam');
+//
+// let index7 = listItem7.text().indexOf('konstantin.dankov@gmail.com');
+// let index8 = listItem8.text().indexOf('@kdankov');
+// let index9 = listItem9.text().indexOf('Konstantin Dankov');
+//
+// let testIndex = index1 + index2 + index3 + index4 + index5 + index6 + index7 + index8 + index9;
+//
+// console.log(testIndex);
