81f2fc7911e4c32ef01359a9fc31583e0232172a
dplamenov <dimitar.plamenov@gmail.com>
2021-12-11 17:16:28 +0200

    Update JavaScriptPlayground.js

--- JavaScriptDevelopment/JavaScriptPlayground.js
+++ JavaScriptDevelopment/JavaScriptPlayground.js
@@ -1,12 +1,33 @@
-function twoStrings(s1, s2) {
-    for (let char of s1) {
-        if (s2.includes(char)) {
-            return 'YES';
-        }
+function minimumNumber(n, password) {
+    const numbers = "0123456789";
+    const lower_case = "abcdefghijklmnopqrstuvwxyz";
+    const upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const special_characters = "!@#$%^&*()-+";
+
+    let needChars = 0;
+
+    if (!/[0-9]/.test(password)) {
+        needChars++;
     }
 
-    return 'NO';
+    if (!/[a-z]/.test(password)) {
+        needChars++;
+    }
+
+    if (!/[A-Z]/.test(password)) {
+        needChars++;
+    }
+
+    if (!/[!@#$%^&*()\-+]/.test(password)) {
+        needChars++;
+    }
+
+    if (n + needChars < 6) {
+        needChars += 6 - (n + needChars);
+    }
+
+    return needChars;
 }
 
-console.log(twoStrings('and', 'art'));
-console.log(twoStrings('be', 'cat'));
+console.log(minimumNumber(3, 'Ab1'));
+console.log(minimumNumber(11, '#HackerRank'));
